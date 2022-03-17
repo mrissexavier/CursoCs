e4da96108ff61c73e1d95a4d9bcae58ed27cd375
mrissexavier <m.risse.dev@gmail.com>
2022-03-17 17:02:11 -0300

    Criando metodos

--- Menus/Menus/Program.cs
+++ Menus/Menus/Program.cs
@@ -1,72 +1,110 @@
-﻿int op = 1;
-while (op != 0)
+﻿namespace Menus
 {
-
-    Console.Clear();
-    Console.WriteLine("Bem vindo! Escolha uma das seguintes opções:");
-    Console.WriteLine("1-Opção A");
-    Console.WriteLine("2-Opção B");
-    Console.WriteLine("3-Opção C");
-    Console.WriteLine("4-Entrar no Submenu");
-    Console.WriteLine("0-Sair");
-    op = int.Parse(Console.ReadLine());
-
-    if (op == 0)
+    class Program
     {
-        break;
-    }
-    else if (op == 1)
-    {
-        Console.Clear();
-        Console.WriteLine("Opção A selecionada");
-        Console.ReadKey();
-    }
-        
-    else if (op == 2)
-    {
-        Console.Clear();
-        Console.WriteLine("Opção B selecionada");
-        Console.ReadKey();
-    }
-    else if (op == 3)
-    {
-        Console.Clear();
-        Console.WriteLine("Opção C selecionada");
-        Console.ReadKey();
-    }
-    else if(op == 4)
-    {
-        Submenu();
-    }
-    else
-    {
-        Console.Clear();
-        Console.WriteLine("Opção inválida");
-        Console.ReadKey();
-    }
-       
-}
-
-void Submenu()
-{
-    int op = 1;
-    while (op != 0)
-    {
-
-        Console.Clear();
-        Console.WriteLine("Submenu");
-        Console.WriteLine("1-Opção A");
-        Console.WriteLine("0-Voltar ao menu principal");
-        op = int.Parse(Console.ReadLine());
-
-        if (op == 0)
+        static void Main(string[] args)
         {
-            break;
+            int op = 1;
+            while (op != 0)
+            {
+
+                Console.Clear();
+                Console.WriteLine("Bem vindo! Escolha uma das seguintes opções:");
+                Console.WriteLine("1-Somar");
+                Console.WriteLine("2-Subtrair");
+                Console.WriteLine("3-Multiplicação");
+                Console.WriteLine("4-Divisão");
+                Console.WriteLine("0-Sair");
+                op = int.Parse(Console.ReadLine());
+
+                if (op == 0)
+                {
+                    break;
+                }
+                else if (op == 1)
+                {
+                    Soma();
+                }
+
+                else if (op == 2)
+                {
+                    Subtracao();
+                }
+                else if(op == 3)
+                {
+                    Multiplicacao();
+                }
+                else if(op == 4)
+                {
+                    Divisao();
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Opção inválida");
+                    Console.ReadKey();
+                }
+
+            }
         }
-        else if (op == 1)
+
+        private static void Divisao()
         {
             Console.Clear();
-            Console.WriteLine("Opção A do submenu selecionada");
+            Console.WriteLine("Insira o primeiro número");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o segundo número");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double div = num1 / num2;
+
+            Console.WriteLine($"A divisão de {num1} por {num2} é: {div}");
+            Console.ReadKey();
+        }
+
+        private static void Multiplicacao()
+        {
+            Console.Clear();
+            Console.WriteLine("Insira o primeiro número");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o segundo número");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double mult = num1 * num2;
+
+            Console.WriteLine($"A multiplicação de {num1} x {num2} é: {mult}");
+            Console.ReadKey();
+        }
+
+        private static void Subtracao()
+        {
+            Console.Clear();
+            Console.WriteLine("Insira o primeiro número");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o segundo número");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double sub = num1 - num2;
+
+            Console.WriteLine($"A subtração de {num1} menos {num2} é: {sub}");
+            Console.ReadKey();
+        }
+
+        private static void Soma()
+        {
+            Console.Clear();
+            Console.WriteLine("Insira o primeiro número");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o segundo número");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double soma = num1 + num2;
+
+            Console.WriteLine($"A soma de {num1} mais {num2} é: {soma}");
             Console.ReadKey();
         }
     }
