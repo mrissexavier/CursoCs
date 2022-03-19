ad4118f3f9f30d93b70e1ef320eb6280ce3df83b
Emily Xavier <m.risse.dev@gmail.com>
2022-03-19 14:09:08 -0300

    exercicio 02

---
+++ ExemploArrays/ExercicioArray02/ExercicioArray02/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ExemploArray02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             * Podemos escrever desta forma:
+             
+              string[] nomes = new string[] {"Emily", "Mark", "Ana", ...};
+
+              Desta forma não precisamos perguntar ao utilizador quais os elementos/nomes com o laço for:
+
+                 for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.WriteLine($"Insira o {i + 1}º nome");
+                nomes[i] = Console.ReadLine();
+            }
+
+             */
+
+            string[] nomes = new string[10];
+
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.WriteLine($"Insira o {i + 1}º nome");
+                nomes[i] = Console.ReadLine();
+            }
+
+            Console.Clear();
+            Console.WriteLine("Qual a posição que pretende verificar?");
+            int posicao = int.Parse(Console.ReadLine())-1;  // -1 para selecionar a posição sem contar o index do Array
+
+            Console.WriteLine(nomes[posicao]);
+            Console.ReadKey();  
+        }
+    }
+}
