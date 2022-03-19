0f525c8f7542b312de49cb28ca4e4a05c7ab1ea8
Emily Xavier <m.risse.dev@gmail.com>
2022-03-19 14:40:33 -0300

    exercicio 03

---
+++ ExemploArrays/ExercicioArray03/ExercicioArray03/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ExercicioArray03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[10];
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Random rnd = new Random();
+
+                numeros[i] = rnd.Next(1,10);
+            }
+            Console.WriteLine("Ordem de inserção: ");
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine($"Posição: {i} \tValor: {numeros[i]}");
+            }
+            Console.WriteLine("=============================================== \n");
+
+            Console.WriteLine("Ordem inversa: ");
+            for (int i = numeros.Length-1; i >= 0; i--)
+            {
+                Console.WriteLine($"Posição: {i} \tValor: {numeros[i]}");
+            }
+        }
+    }
+}
