9ddacb4f26549c5c1a0759965bb78d6a7fa56223
Emily Xavier <m.risse.dev@gmail.com>
2022-03-21 15:13:18 -0300

    Replace(String, String)

---
+++ MetodoString/Ex004/Ex004/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ex003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+             * Replace(Char, Char)	=
+            * Retorna uma nova cadeia de caracteres na qual todas as ocorrências de um caractere 
+            * Unicode especificado nessa instância são substituídas por outro caractere Unicode especificado.
+             */
+
+            Console.WriteLine("Digite seu nome completo:");
+            string nome = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("Qual letra deseja remover do nome?");
+            string letraEscolhida = Console.ReadLine();
+
+            nome = nome.Replace(letraEscolhida, "");
+
+            Console.WriteLine($"Bem vindo(a), {nome}");
+            Console.ReadKey();
+        }
+    }
+}
