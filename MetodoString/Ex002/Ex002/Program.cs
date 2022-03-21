a2dc9fbce0243932097ff7c83808cef219753d83
Emily Xavier <m.risse.dev@gmail.com>
2022-03-21 14:33:30 -0300

    ToLower

---
+++ MetodoString/Ex002/Ex002/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Ex002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome completo:");
+            string nome = Console.ReadLine();
+            nome = nome.ToLower();
+
+            Console.WriteLine($"Bem vindo, {nome}!");
+        }
+    }
+}
