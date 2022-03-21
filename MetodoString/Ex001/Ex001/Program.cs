38df5c40a897566f1934d27399bc7e8e0cb8f04f
Emily Xavier <m.risse.dev@gmail.com>
2022-03-21 13:07:02 -0300

    metodos string

---
+++ MetodoString/Ex001/Ex001/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Ex001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ToUpper() = Retorna uma cópia dessa cadeia de caracteres convertida em maiúsculas.
+
+            Console.WriteLine("Eai, como tu se chama?");
+            string nome = Console.ReadLine();
+            nome = nome.ToUpper();
+
+            Console.WriteLine($"Chega mais, {nome} :)");
+        }
+    }
+}
