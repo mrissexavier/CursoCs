bc36d13147cce11d30f8a1c18dc75156701efac1
Emily Xavier <m.risse.dev@gmail.com>
2022-03-21 15:50:23 -0300

    Replace + laco while

---
+++ MetodoString/Ex005/Ex005/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Ex005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+             * Replace(String, String)	=
+            * Retorna uma nova cadeia de caracteres na qual todas as ocorrências de um caractere 
+            * Unicode especificado nessa instância são substituídas por outro caractere Unicode especificado.
+             */
+
+            Console.WriteLine("Digite seu nome completo:");
+            string nome = Console.ReadLine();
+
+            Console.Clear();
+
+            while(nome.Length > 0)
+            {
+                Console.WriteLine("Qual letra deseja remover do nome?");
+                string letraEscolhida = Console.ReadLine();
+
+                nome = nome.Replace(letraEscolhida, "");
+
+                Console.WriteLine($"Bem vindo(a), {nome}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
