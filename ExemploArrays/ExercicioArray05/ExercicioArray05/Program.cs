9441b56afeb57aebffedd252d730f196f13787ff
Emily Xavier <m.risse.dev@gmail.com>
2022-03-21 12:30:20 -0300

    exercicio 05

--- ExemploArrays/ExercicioArray05/ExercicioArray05/Program.cs
+++ ExemploArrays/ExercicioArray05/ExercicioArray05/Program.cs
@@ -19,15 +19,14 @@
 
             // Ordenação crescente
             Console.WriteLine(" =================== Ordem Crescente ===================== ");
-                                         // Utilizando Bubble Sort para ordenar
+                                                                   // Utilizando Bubble Sort para ordenar
             //Crescente();
             Array.Sort(numeros); // <-- faz a mesma função do método Crescente();
             Imprimir();
 
             //Ordenação decrescente
             Console.WriteLine(" =================== Ordem Decrescente ===================== ");
-            
-           //Decrescente();
+            //Decrescente();
             Array.Sort(numeros);
             Array.Reverse(numeros); // pega nossa ordenação e coloca na ordem reversa
             Imprimir();
