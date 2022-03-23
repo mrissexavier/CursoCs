786fd3fb26fc0c7426e3730a961ffa20c3074e9d
Emily Xavier <m.risse.dev@gmail.com>
2022-03-23 08:32:12 -0300

    Split(Char[])

---
+++ MetodoString/Ex008/Ex008/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ex008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Split(Char[])	
+             * Divide uma cadeia de caracteres em substrings baseadas nos caracteres delimitadores especificados.
+             */
+
+            Console.WriteLine("Digite seu nome");
+            string nome = Console.ReadLine();
+
+            string[] nomes = nome.Split(" ");
+
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.WriteLine(nomes[i]);
+            }
+
+        }
+    }
+}
