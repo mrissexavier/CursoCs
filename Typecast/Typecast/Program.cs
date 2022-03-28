133f0913ab0e34193d2d84c9a686d547d32a0606
Emily Xavier <m.risse.dev@gmail.com>
2022-03-28 16:00:21 -0300

    Typecast

---
+++ Typecast/Typecast/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Typecast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float  num1 = 10.5f;
+            int num2 = (int)num1;
+
+            Console.WriteLine(num1);
+        }
+    }
+}
