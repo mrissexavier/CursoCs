119a45d0185e7ecce5ddc75693be79ab4840e190
Emily Xavier <m.risse.dev@gmail.com>
2022-03-28 15:17:55 -0300

    Bitewise

---
+++ Bitwise/Bitwise/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Bitwise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10, num1 = 20;
+            /* 
+                " << " dobra o valor 
+                " >> " divide o valor
+             */
+            num = num << 1;
+            num1 = num1 >> 1;
+
+            Console.WriteLine(num);
+            Console.WriteLine(num1);
+        }
+    }
+}
