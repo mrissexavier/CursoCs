143c59f17c1a9728a50e83079921646f3aa8f4ab
Emily Xavier <m.risse.dev@gmail.com>
2022-03-28 10:25:06 -0300

    Criação de objeto

---
+++ POO/POO/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    internal class Pessoa
+    {
+        string nome;
+        string cidade;
+        int idade;
+        string email;
+
+        public Pessoa()
+        {
+            nome = "";
+            cidade = "";
+            idade = 0;
+            email = "";
+        }
+
+        public Pessoa(string nome, string cidade, int idade, string email)
+        {
+            this.nome = nome;
+            this.cidade = cidade;
+            this.idade = idade;
+            this.email = email;
+        }
+
+        public override string? ToString()
+        {
+            return ($"Nome: {nome}" +
+                $"\nCidadae: {cidade}" +
+                $"\nIdade: {idade}" +
+                $"\nEmail: {email}\n");
+        }
+    }
+}
---
+++ POO/POO/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa();
+            Pessoa pessoa2 = new Pessoa("Mark", "São Paulo", 22, "mark.dev@gmail.com");
+
+            Console.WriteLine(pessoa1.ToString());
+            Console.WriteLine("============================");
+            Console.WriteLine(pessoa2.ToString());
+        }
+    }
+}
