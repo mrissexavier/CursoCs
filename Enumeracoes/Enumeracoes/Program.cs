c9bcbdec7099ba234b689e1e1074a993e26c9eb4
Emily Xavier <m.risse.dev@gmail.com>
2022-04-19 18:50:05 -0300

    Enum

---
+++ Enumeracoes/Enumeracoes/Program.cs
@@ -0,0 +1,28 @@
+﻿using Enumeracoes.Entities;
+using Enumeracoes.Entities.Enums;
+
+namespace Enumeracoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString(); // convertendo enum para string
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered"); // convertendo string para enum
+
+            Console.WriteLine(os);
+
+            Console.WriteLine(txt);
+        }
+    }
+}
---
+++ ExemploArrays/MetodosArray/MetodosArray/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[] vetor1 = new int[5];
+        int[] vetor2 = new int[5];
+        int[] vetor3 = new int[5];
+        int[,] matriz = new int[2, 5] { { 11,22,00,44,55}, { 66,77,88,99,00} };
+
+        Random rnd = new Random();
+
+    }
+}
