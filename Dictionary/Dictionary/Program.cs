20ab9eeca0becd5084e7ad6f71a97b60d967e9a5
Emily Xavier <m.risse.dev@gmail.com>
2022-04-30 18:30:33 -0300

    Dictionary

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["user"] = "Maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "99712234";
+
+            Console.WriteLine(cookies["user"]);
+            Console.WriteLine(cookies["email"]);
+
+            cookies.Remove("email");
+
+            if (cookies.ContainsKey("email"))
+            {
+                Console.WriteLine(cookies["email"]);
+            }
+            else
+            {
+                Console.WriteLine("There is no 'email' key");
+            }
+
+            Console.WriteLine(cookies.Count); 
+
+            Console.WriteLine("All cookies: ");
+            foreach(KeyValuePair<string, string> kvp in cookies)
+            {
+                Console.WriteLine(kvp.Key + ": " + kvp.Value);
+            }
+        }
+    }
+}
