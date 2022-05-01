091d3d21fdf63c82b7bcdbf6b5a96e483a71bffb
Emily Xavier <m.risse.dev@gmail.com>
2022-05-01 13:40:33 -0300

    Delegates

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,21 @@
+﻿using Delegates.Services;
+
+namespace Delegates
+{
+
+    delegate double BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 20;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+            double result = op(a, b);
+
+            Console.WriteLine(result);
+   
+        }
+    }
+}
---
+++ Delegates/Delegates/Services/CalculationService.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace Delegates.Services
+{
+    internal class CalculationService
+    {
+        public static double Max(double x, double y)
+        {
+            return (x > y) ? x : y;
+        }
+        public static double Sum(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Square(double x)
+        {
+            return x * x;
+        }
+    }
+}
+
